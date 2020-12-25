28c1e3667d038efed7d5cbb82344832073a9a009
Lanka-92791 <75198714+Lanka-92791@users.noreply.github.com>
2020-12-25 21:42:35 +0530

    User files

--- javaconnect.java
+++ javaconnect.java
@@ -13,23 +13,19 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author TW PC
+ * @author asd
  */
 class javaconnect {
-    
-    private static Connection connection;
-
-    static Connection ConnecrDb() {
-        if (connection == null){
-            try {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "root", "1234");
-            } catch (SQLException ex) {
-                Logger.getLogger(javaconnect.class.getName()).log(Level.SEVERE, null, ex);
-            }
+static Connection con;
+   public static void ConnecrDb() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/librarysystem","root","");// "onlineexam" this is database name
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(javaconnect.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(javaconnect.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return connection;
-        
-    }
-    
+   }
     
 }
